a9d95127dcf57b45a82006ce70dc127f69508aa6
Kosmos <sergey.badyanov1@mail.ru>
2023-09-14 15:27:44 +0500

    Comment

--- app/local/repository/main_repository.py
+++ app/local/repository/main_repository.py
@@ -10,13 +10,13 @@ engine = create_engine('postgresql://postgres:89080620743@localhost:5432/parsing
 connection = engine.connect()
 metadata = db.MetaData()
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-# Создание таблицы в базе данных
+# Создание таблицы в базе данных!
 Base.metadata.create_all(engine)
-# проверка существует ли ссылка в базне данных по базовому URL
+# проверка существует ли ссылка в базне данных по базовому URL!
 def check_existing_link(session: Session, link: str) -> Parsing_Data:
     existing_link = session.query(Parsing_Data).filter(Parsing_Data.url == link).first()
     return existing_link
-#  Создает новую запись Parsing_Data, если ссылка не существует в базе данных.
+#  Создает новую запись Parsing_Data, если ссылка не существует в базе данных!
 def create_new_link(session: Session, base_url: str, link: str) -> None:
     existing_link = check_existing_link(session, link)
     if existing_link is None:
